cd374b2a6749932deec85a733938d3ed05692461
MichaelFetene123 <michaelfetene0202@gmail.com>
2026-08-22 13:42:59 +0300

    feat: create ProductImage model with associations for products and variants

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'product_variant_id',
+        'path',
+        'sort_order',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function variant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
 }
